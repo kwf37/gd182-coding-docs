6977a06d116795e0c856426dde7be5c291effa53
Kenneth Fang <fangkw42@gmail.com>
2026-04-25 23:58:20 -0700

    Remove editing from repo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,9 +43,6 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           routeBasePath: '/', // Set this value to '/'.
           // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: false,
         theme: {
